ddfb77d9b16075ceaad11784ad685cf310011baa
Karen Kepner <kkepner14 (at) gmail.com>
2019-02-07 10:43:38 -0700

    create Player class and instantiate new player object

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.speed = speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +28,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+    this.player = 'images/char-horn-girl.png';
+}
 
+Player.prototype.update = function() {
+    console.log('the Player.prototype.update method has been called');
+};
 
+Player.prototype.render = function() {
+    
+};
+
+Player.prototype.handleInput = function() {
+    
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+let allEnemies = [];
+let player =  new Player; //awesome.
 
 
 // This listens for key presses and sends the keys to your
